e4c9cd9293443d445bfa84b5b5ea6be7329b8999
ysrotciv <ysrotciv@gmail.com>
2016-05-12 10:02:13 +0800

    添加淘股吧的解析

---
+++ dispatcher/src/main/scala/com/kunyan/dispatcher/parser/TaogubaParser.scala
@@ -0,0 +1,65 @@
+package com.kunyan.dispatcher.parser
+
+import org.jsoup.Jsoup
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by yangshuai on 2016/5/11.
+  */
+object TaogubaParser {
+
+  /**
+    * 淘股吧解析类入口
+    *
+    * @param html 要解析的页面信息字符串
+    * @return 返回排名前五的帖子的id和title信息集合
+    */
+  def parse(html: String): ListBuffer[String] = {
+
+    var idTitle = ListBuffer[String]()
+    val doc = Jsoup.parse(html, "UTF-8")
+    val map = collection.mutable.Map[String, String]()
+
+    try {
+      val list = doc.select("div.p_list01 li.pcdj02")
+
+      for (i <- 0 until list.size) {
+
+        val numText = list.get(i).select("b").text()
+        val num = numText.substring(1, numText.length - 1)
+        map.put(i.toString, num)
+
+      }
+
+      var sortList = ListBuffer[String]()
+
+      map.toArray.sortBy(_._2) foreach {
+        x => {
+          sortList = sortList ++ ListBuffer(x._1)
+        }
+      }
+
+      val result: ListBuffer[String] = sortList.reverse.take(5)
+
+      for (j <- result.indices) {
+
+        val children = list.get(result(j).toInt)
+        val title = children.select("a").text()
+        val id = children.select("a").attr("href").split("/")(1)
+        idTitle = idTitle ++ ListBuffer(id + "|||" + title)
+
+      }
+
+      idTitle
+
+    } catch {
+        case e: Exception =>
+          e.printStackTrace()
+          null
+      }
+
+    }
+
+
+  }
--- dispatcher/src/main/scala/com/kunyan/dispatcher/scheduler/Scheduler.scala
+++ dispatcher/src/main/scala/com/kunyan/dispatcher/scheduler/Scheduler.scala
@@ -4,22 +4,21 @@ package com.kunyan.dispatcher.scheduler
 import java.util.Date
 
 import _root_.kafka.serializer.StringDecoder
-import com.kunyan.dispatcher.config.{Platform, LazyConnections}
-import com.kunyan.dispatcher.parser.BaiduParser
+import com.kunyan.dispatcher.config.{LazyConnections, Platform}
+import com.kunyan.dispatcher.parser.{BaiduParser, TaogubaParser}
 import org.apache.hadoop.hbase.client.Get
 import org.apache.hadoop.hbase.util.Bytes
 import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.SparkConf
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import org.apache.commons.codec.digest.DigestUtils
-
 
 import scala.util.parsing.json.JSON
 
 /**
   * Created by yangshuai on 2016/4/26.
-  * XX项目主流程类
+  * 机器人项目中消息处理的主流程类
   */
 object Scheduler {
 
@@ -51,35 +50,21 @@ object Scheduler {
         val messageMap = parseJson(x._2)
 
         try {
+
           val tableName = messageMap.get("key_name").get
           val rowkey = messageMap.get("pos_name").get
+          val attrId = messageMap.get("attr_id").get
 
           val dbResult = getHtml(tableName, rowkey, lazyConnBr.value)
           val originUrl = dbResult._1
           val html = dbResult._2
 
-          if (originUrl.startsWith("http://tieba.baidu.com/f/index/forumpark")) {
-
-            val messages = BaiduParser.topBars(html).map(getUrlJsonString)
-            lazyConnBr.value.sendTask("robot_stock", messages.toSeq)
-
-          } else if (originUrl.startsWith("http://tieba.baidu.com/f?kw=")) {
-
-            val messages = BaiduParser.getHotPosts(html).map(getUrlJsonString)
-            println(messages)
-            lazyConnBr.value.sendTask("robot_stock", messages.toSeq)
-
-          } else if (originUrl.startsWith("http://tieba.baidu.com/p/")) {
-
-            val tuple = BaiduParser.getUserInfo(html)
-
-            if (tuple != null) {
-
-              lazyConnBr.value.sendTask("robot_tiebacomment", getCommentJsonString(originUrl, tuple))
-              lazyConnBr.value.sendTask("robot_tiebacomment", getCommentJsonString(originUrl, (tuple._1, tuple._2, tuple._3, "")))
-
-            }
+          if (attrId.toInt == Platform.Tieba.id) {
+            parseTieba(lazyConnBr, originUrl, html)
+          } else if (attrId.toInt == Platform.Taoguba.id) {
+            parseTaoguba(lazyConnBr, originUrl, html)
           }
+
         } catch {
           case e: Exception =>
             e.printStackTrace()
@@ -94,42 +79,111 @@ object Scheduler {
   }
 
   /**
-    * 拼接json字符串
+    * 处理淘股吧消息
+    *
+    * @param lazyConnBr 连接容器
+    * @param originUrl hbase中取出的url
+    * @param html hbase中取出的html
+    */
+  def parseTaoguba(lazyConnBr: Broadcast[LazyConnections], originUrl: String, html: String): Unit = {
+    if (originUrl.startsWith("http://www.taoguba.com.cn")) {
+
+      val messages = TaogubaParser.parse(html)
+      println(messages)
+      messages.foreach {
+        x => {
+
+          val id = x.split("|||")(0)
+          val title = x.split("|||")(1)
+          if (id.nonEmpty && title.nonEmpty) {
+            lazyConnBr.value.sendTask("robot_tiebacomment", getCommentJsonString(id, title))
+
+          }
+        }
+      }
+
+    }
+  }
+
+  /**
+    * 处理百度贴吧消息
+    *
+    * @param lazyConnBr 连接容器
+    * @param originUrl hbase中取出的url
+    * @param html hbase中取出的html
+    */
+  def parseTieba(lazyConnBr: Broadcast[LazyConnections], originUrl: String, html: String): Unit = {
+
+    if (originUrl.startsWith("http://tieba.baidu.com/f/index/forumpark")) {
+
+      val messages = BaiduParser.topBars(html).map(getUrlJsonString)
+      lazyConnBr.value.sendTask("robot_stock", messages.toSeq)
+
+    } else if (originUrl.startsWith("http://tieba.baidu.com/f?kw=")) {
+
+      val messages = BaiduParser.getHotPosts(html).map(getUrlJsonString)
+      println(messages)
+      lazyConnBr.value.sendTask("robot_stock", messages.toSeq)
+
+    } else if (originUrl.startsWith("http://tieba.baidu.com/p/")) {
+
+      val tuple = BaiduParser.getUserInfo(html)
+
+      if (tuple != null) {
+
+        lazyConnBr.value.sendTask("robot_tiebacomment", getCommentJsonString(originUrl, tuple))
+        lazyConnBr.value.sendTask("robot_tiebacomment", getCommentJsonString(originUrl, (tuple._1, tuple._2, tuple._3, "")))
+
+      }
+    }
+  }
+
+  /**
+    * 拼接往robot_stock topic 发的消息的json字符串
+    *
+    * @param url 消息中的帖子的url
+    * @return json格式的消息的字符串
     */
   def getUrlJsonString(url: String): String = {
 
     val json = "{\"id\":\"\", \"attrid\":\"%d\", \"cookie\":\"\", \"referer\":\"\", \"url\":\"%s\", \"timestamp\":\"%s\"}"
 
     json.format(Platform.Tieba.id, url, new Date().getTime.toString)
-
   }
 
   /**
     * 拼接百度贴吧回帖json
     *
-    * @param url 回帖地址
+    * @param url   回帖地址
     * @param tuple 回帖所需参数
-    * @return
+    * @return json格式的消息的字符串
     */
   def getCommentJsonString(url: String, tuple: (String, String, String, String)): String = {
 
     val json = "{\"plat_id\":%d, \"preUrl\":\"%s\", \"kw\":\"%s\", \"fid\":\"%s\", \"tbs\":\"%s\", \"repostid\":\"%s\", \"timestamp\":\"%s\"}"
-    json.format(Platform.Tieba.id, url, tuple._1, tuple._2, tuple._3, tuple._4, new Date().getTime.toString)
 
+    json.format(Platform.Tieba.id, url, tuple._1, tuple._2, tuple._3, tuple._4, new Date().getTime.toString)
   }
 
   /**
-    * 拼接json字符串
+    * 拼接淘股吧回帖json
+    *
+    * @param id 帖子id
+    * @param title 帖子标题
+    * @return json格式的消息的字符串
     */
-  def getCommentJsonString(url: String, barName: String, fid: String, tbs: String): String = {
+  def getCommentJsonString(id: String, title: String): String = {
 
-    val json = "{\"pre_url\":\"%s\", \"kw\":\"%s\", \"fid\":\"%s\", \"floor_num\":\"1\", \"tbs\":\"%s\", \"content\":\"\", \"repostid\":\"\", \"timestamp\":\"%s\"}"
-    json.format(url, barName, fid, tbs, new Date().getTime.toString)
+    val json = "{\"plat_id\":%d, \"id\":\"%s\", \"title\":\"%s\", \"timestamp\":\"%s\"}"
 
+    json.format(Platform.Taoguba.id, id, title, new Date().getTime.toString)
   }
 
   /**
     * 从json字符串中解析数据
+    *
+    * @param source json字符串
+    * @return map形式的json数据
     */
   def parseJson(source: String): Map[String, String] = {
 
@@ -139,9 +193,7 @@ object Scheduler {
       val json: Option[Any] = JSON.parseFull(source)
 
       if (json.isDefined) {
-
         val map: Map[String, String] = json.get.asInstanceOf[Map[String, String]]
-
         map
       } else {
         null
@@ -153,15 +205,25 @@ object Scheduler {
     }
   }
 
+  /**
+    * 根据表名和rowkey从hbase中获取数据
+    *
+    * @param tableName 表名
+    * @param rowkey 索引
+    * @param lazyConn 连接容器
+    * @return (url, html)
+    */
   def getHtml(tableName: String, rowkey: String, lazyConn: LazyConnections): (String, String) = {
 
     val table = lazyConn.getTable(tableName)
     val get = new Get(rowkey.getBytes)
 
     try {
+
       val result = table.get(get)
       val url = new String(result.getValue(Bytes.toBytes("basic"), Bytes.toBytes("url")))
       val content = new String(result.getValue(Bytes.toBytes("basic"), Bytes.toBytes("content")))
+
       (url, content)
     } catch {
       case e: Exception =>
