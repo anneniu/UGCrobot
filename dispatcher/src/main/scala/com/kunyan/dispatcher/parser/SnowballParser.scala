6d89694adb4600832aa21f5a9735e1812e053b4b
anneniu <niujiaojiao@kunyan-inc.com>
2016-05-18 18:10:49 +0800

    add try catch

--- dispatcher/src/main/scala/com/kunyan/dispatcher/parser/SnowballParser.scala
+++ dispatcher/src/main/scala/com/kunyan/dispatcher/parser/SnowballParser.scala
@@ -17,6 +17,7 @@ object SnowballParser {
     */
   def parse(html: String): ListBuffer[String] = {
 
+    var result = ListBuffer[String]()
     val pageUrl = "https://xueqiu.com"
 
     getNews(html, pageUrl)
@@ -34,28 +35,36 @@ object SnowballParser {
     var result = ListBuffer[String]()
     val jsonInfo = JSON.parseFull(json)
 
-    if (jsonInfo.isEmpty) {
-      println("\"JSON parse value is empty,please have a check!\"")
-    } else {
+    try {
 
-      jsonInfo match {
+      if (jsonInfo.isEmpty) {
+        println("\"JSON parse value is empty,please have a check!\"")
+      } else {
 
-        case Some(mapInfo: List[Map[String, AnyVal]]) =>
+        jsonInfo match {
 
-          for (i <- mapInfo.indices) {
-            val url = mapInfo(i).getOrElse("target", "")
-            val total = "https://xueqiu.com" + url
-            result += total
-          }
+          case Some(mapInfo: List[Map[String, AnyVal]]) =>
 
-        case None => println("Parsing failed!")
-        case other => println("Unknown data structure :" + other)
+            for (i <- mapInfo.indices) {
+              val url = mapInfo(i).getOrElse("target", "")
+              val total = "https://xueqiu.com" + url
+              result += total
+            }
+
+          case None => println("Parsing failed!")
+          case other => println("Unknown data structure :" + other)
+
+        }
 
       }
 
-    }
+      result
 
-    result
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        null
+    }
 
   }
 
@@ -70,17 +79,26 @@ object SnowballParser {
 
     val doc = Jsoup.parse(html, "UTF-8")
     var result = ListBuffer[String]()
-    val list = doc.select("div#center div.list_item  div.list_item_rb  div.list_item_tit a[href]")
 
-    for (i <- 0 until list.size) {
+    try {
 
-      val url = list.get(i).attr("href")
-      val total = preurl + url
-      result += total
+      val list = doc.select("div#center div.list_item  div.list_item_rb  div.list_item_tit a[href]")
 
+      for (i <- 0 until list.size) {
+
+        val url = list.get(i).attr("href")
+        val total = preurl + url
+        result += total
+
+      }
+
+      result
+
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+        null
     }
-
-    result
 
   }
 
