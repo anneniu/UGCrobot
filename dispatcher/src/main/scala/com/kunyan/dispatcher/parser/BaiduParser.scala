eccfa4b104adfc02332cc08669e8e343b5ee0443
anneniu <niujiaojiao@kunyan-inc.com>
2016-05-24 09:13:02 +0800

    Update baiduParser:

--- dispatcher/src/main/scala/com/kunyan/dispatcher/parser/BaiduParser.scala
+++ dispatcher/src/main/scala/com/kunyan/dispatcher/parser/BaiduParser.scala
@@ -24,6 +24,7 @@ object BaiduParser {
     }
 
     set
+
   }
 
   def getHotPosts(html: String): mutable.Set[String] = {
@@ -32,42 +33,53 @@ object BaiduParser {
 
     val map = mutable.Map[Int, Element]()
 
-    val doc = Jsoup.parse(html, "UTF-8")
+    val removeStr = html.replace("<!--","").replace("-->","")
+    val doc = Jsoup.parse(removeStr, "UTF-8")
 
     var tags: Elements = null
 
     try {
-      tags = doc.getElementById("thread_list").getElementsByAttributeValue("class", " j_thread_list clearfix")
-    } catch {
-      case e: NullPointerException =>
-        tags = Jsoup.parse(doc.getElementById("pagelet_html_frs-list/pagelet/thread_list").toString.split("<!--")(1).split("-->")(0).trim()).getElementById("thread_list").getElementsByAttributeValue("class", " j_thread_list clearfix")
-    }
 
-    for (i <- 0 until tags.size()) {
-
-      val iTag = tags.get(i)
-
-      try {
-
-        val readCount = StringToInt(iTag.getElementsByAttributeValue("class", "row").get(0).getElementsByTag("span").get(1).text)
-        val commentCount = StringToInt(iTag.getElementsByAttributeValue("class", "row").get(1).getElementsByTag("span").get(1).text)
-
-        map.put(readCount + commentCount * 2, iTag)
-      } catch {
-        case e: Exception =>
-          val replyCount = StringToInt(iTag.getElementsByAttributeValue("class", "threadlist_rep_num center_text").get(0).text)
-          map.put(replyCount, iTag)
+      if(doc.toString.isEmpty){
+        return null
       }
 
+      tags = doc.getElementById("thread_list").getElementsByAttributeValue("class", " j_thread_list clearfix")
+
+      if(tags.isEmpty){
+        return null
+      }
+
+      for (i <- 0 until tags.size()) {
+
+        val iTag = tags.get(i)
+
+        try {
+
+          val readCount = StringToInt(iTag.getElementsByAttributeValue("class", "row").get(0).getElementsByTag("span").get(1).text)
+          val commentCount = StringToInt(iTag.getElementsByAttributeValue("class", "row").get(1).getElementsByTag("span").get(1).text)
+
+          map.put(readCount + commentCount , iTag)
+        } catch {
+          case e: Exception =>
+            val replyCount = StringToInt(iTag.getElementsByAttributeValue("class", "threadlist_rep_num center_text").get(0).text)
+            map.put(replyCount, iTag)
+        }
+
+      }
+
+      val sortedMap = map.toSeq.sortWith(_._1 > _._1)
+      for (i <- 0 until 3) {
+        val url = sortedMap(i)._2.getElementsByTag("a").attr("href")
+        set.add("http://tieba.baidu.com" + url)
+      }
+
+      set
+    } catch {
+      case e: NullPointerException =>
+        null
     }
 
-    val sortedMap = map.toSeq.sortWith(_._1 > _._1)
-    for (i <- 0 until 3) {
-      val url = sortedMap(i)._2.getElementsByTag("a").attr("href")
-      set.add("http://tieba.baidu.com" + url)
-    }
-
-    set
   }
 
   def getFirstPost(html: String): String = {
@@ -87,21 +99,24 @@ object BaiduParser {
     try {
 
       var pid = ""
-      var barName = title.split("_")(1)
-      barName = barName.substring(0, barName.length - 1)
-      val fid = html.split("fid: '")(1).split("'")(0)
-      val tbs = html.split("tbs:'")(1).split("',")(0)
-      val text = doc.select("cc div").get(1).toString
+      if(title.contains("_")){
+        var barName = title.split("_")(1)
+        barName = barName.substring(0, barName.length - 1)
+        val fid = html.split("fid: '")(1).split("'")(0)
+        val tbs = html.split("tbs:'")(1).split("',")(0)
 
-      if (text.nonEmpty) {
-        pid = text.split("content_")(1).split("\"")(0)
-      }
-
-      (barName, fid, tbs, pid)
-
+        if(doc.select("cc div").size >=2){
+          val text = doc.select("cc div").get(1).toString
+          if (text.nonEmpty && text.contains("content_")) {
+            pid = text.split("content_")(1).split("\"")(0)
+            (barName, fid, tbs, pid)
+          } else {
+            null
+          }
+        }else null
+      } else null
     } catch {
       case e: Exception =>
-        e.printStackTrace()
         null
     }
 
@@ -115,4 +130,5 @@ object BaiduParser {
       text.toInt
     }
   }
+
 }
